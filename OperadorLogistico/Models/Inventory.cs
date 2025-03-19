ca18f6cd1aea987015d020db48a1f9d19c85b02b
Mauricio Melis <mmelis@seidor.es>
2025-03-19 16:09:50 +0100

    Agregar archivos de proyecto.

---
+++ OperadorLogistico/Models/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadorLogistico.Models
+{
+    public class Inventory
+    {
+        public string CodigoProducto { get; set; }
+        public Producto Producto { get; set; }
+        public decimal CantidadDisponible { get; set; }
+        public ICollection<Lote> Lotes { get; set; }
+        public DateTime UltimaActualizacion { get; set; }
+        public string Almacen { get; set; }
+    }
+}
---
+++ OperadorLogistico/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadorLogistico.Models
+{
+    public class Product
+    {
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string UnidadMedida { get; set; }
+        public decimal PrecioUnitario { get; set; }
+    }
+}
