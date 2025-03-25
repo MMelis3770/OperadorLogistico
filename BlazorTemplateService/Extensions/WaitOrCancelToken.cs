1c857281a99aff002cc31c0f17f838961574dfff
Mauricio Melis <mmelis@seidor.es>
2025-03-25 16:25:32 +0100

    Blazor Template Service implementation

---
+++ BlazorTemplateService/Extensions/WaitOrCancelToken.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazorTemplateService.Extensions
+{
+    public static class WaitOrCancel
+    {
+        public static async Task<T> WithTimeOutAsyncWithReturn<T>(int milliSeconds, Func<T> function)
+        {
+            using (var cts = new CancellationTokenSource())
+            {
+                cts.CancelAfter(milliSeconds);
+
+                try
+                {
+                    return await Task.Run(function.Invoke, cts.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    throw new TimeoutException();
+                }
+            }
+        }
+    }
+}
