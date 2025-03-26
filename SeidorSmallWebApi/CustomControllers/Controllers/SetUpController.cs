4e07e905500a8872abe3492704e77763c93ab52d
Martí Corder Crivillés <marti.corder@seidor.com>
2025-03-26 16:56:53 +0100

    Endpoint creat

---
+++ SeidorSmallWebApi/CustomControllers/Controllers/SetUpController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SEIDOR_SLayer;
+using SeidorSmallWebApi.Initialize;
+using SeidorSmallWebApi.Routes;
+using System.Threading.Tasks;
+
+namespace SeidorSmallWebApi.Controllers
+{
+    [ApiController]
+    [Authorize] // Protegeix la endpoint perquè només pugui ser cridada per usuaris autenticats
+    [Route($"{ApiRoutes.BaseRoute}/[controller]")]
+    public class SetupController : ControllerBase
+    {
+        private readonly SEI_CreateTablesSL _createTablesSL;
+
+        public SetupController(SEI_CreateTablesSL createTablesSL)
+        {
+            _createTablesSL = createTablesSL;
+        }
+
+        [HttpPost("initialize")]
+        public async Task<IActionResult> Initialize()
+        {
+            try
+            {
+                await _createTablesSL.CreateAllObjects();
+                return Ok(new { message = "Initialization completed successfully." });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { error = ex.Message });
+            }
+        }
+    }
+}
