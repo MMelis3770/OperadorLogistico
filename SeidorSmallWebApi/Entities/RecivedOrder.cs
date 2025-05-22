b0e3e7940e1a30f2bec92a5cb6f7d63cc6148fe9
pfont2 <pfont2@ESONEPC0JFN2T>
2025-05-22 16:54:15 +0200

    Make isProcessed and hasError nullable

--- SeidorSmallWebApi/Entities/RecivedOrder.cs
+++ SeidorSmallWebApi/Entities/RecivedOrder.cs
@@ -6,8 +6,8 @@
         public string client { get; set; }
         public DateTime orderDate { get; set; }
         public DateTime dueDate { get; set; }
-        public int isProcessed { get; set; }
-        public int hasError { get; set; }
+        public int? isProcessed { get; set; }
+        public int? hasError { get; set; }
         public string? errorMessage { get; set; }
         public List<RecivedOrderLines> lines { get; set; }
     }
